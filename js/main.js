c10164ea5263031a8f0a64022add4f555438bb45
Rushabh Padalia <padalia.rushabh@gmail.com>
2015-01-24 00:51:34 -0600

    added resume.html and displayed on html button clicked

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-  $('.contact').tooltip();
+  	$('.contact').tooltip();
+  
+  	/*Resume*/
+  
+  	var isdisplayed = false; 
+  	$('#resume-html-btn').click(function(){
+
+  		if(isdisplayed == false){
+  				console.log("clicked");
+				$('#resume-html-display').load("resume.html");
+  				isdisplayed = true;
+		} else{
+			$('#resume-html-display').html("");
+			isdisplayed = false;		
+		}
+  	});
 });
