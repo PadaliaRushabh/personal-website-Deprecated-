5d4bc7899ccc3463e1de7ab6ca39521d865f0977
Rushabh Padalia <padalia.rushabh@gmail.com>
2015-02-01 11:28:12 -0600

    resume routes and error handaling with custom message

--- pws/app.js
+++ pws/app.js
@@ -11,6 +11,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+var html_dir = './html/';
+app.locals.json = require(__dirname + "/config/data/data.json");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,21 +24,64 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'html')));
 
 app.use('/', routes);
 app.use('/mail', mail);
 //app.use('/mail', function (req , res , next) {console.log("test");})
 
-app.locals.json = require(__dirname + "/config/data/data.json");
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/resume', function(req, res){
+ 
+  res.redirect('/resume/html');
 });
 
-// error handlers
+//serve static html resume file
+app.get('/resume/html', function(req, res) {
+	 var options = {
+    	root: __dirname + '/html',
+    	dotfiles: 'deny',
+    	headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    	}
+  	};
+    res.sendFile("resume.html", options);
+});
+
+app.get('/resume/doc', function(req, res){
+  var file = __dirname + '/public/resume/Rushabh_Padalia_Resume.docx';
+  res.download(file); // Set disposition and send it.
+});
+
+app.get('/resume/pdf', function(req, res){
+  var file = __dirname + '/public/resume/Rushabh_Padalia_Resume.pdf';
+  res.download(file); // Set disposition and send it.
+});
+
+app.get('/resume/*', function(req, res){
+  res.redirect('/resume/html');
+});
+
+//catch 404 error
+app.get('*', function(req, res, next) {
+  var err = new Error();
+  err.status = 404;
+  next(err);
+});
+
+// catch 404 and forward to error handler
+app.use(function(err, req, res, next) {
+	if(err.status !== 404){
+		return next();	
+	}
+	
+	res.render("404");
+});
+
+app.use(function(err, req, res,next){
+	res.status(500);
+	res.send('opps! something broke');
+});
 
 // development error handler
 // will print stacktrace
